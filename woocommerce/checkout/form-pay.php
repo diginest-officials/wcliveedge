c1515628e39404b0380764d476ca376ecdd2e371
Haroon Yamin <haroon.webdev@gmail.com>
2025-08-23 12:46:45 +0500

    Update version of woocommerce template

--- woocommerce/checkout/form-pay.php
+++ woocommerce/checkout/form-pay.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * Pay for order form
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-pay.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 8.2.0
- */
-
 defined( 'ABSPATH' ) || exit;
 
 $totals = $order->get_order_item_totals(); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
